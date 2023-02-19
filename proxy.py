f405076110635adc0f368db20bf75ce470ce6ac8
Shane Caldlwell <shanejcaldwell@gmail.com>
2023-02-19 11:35:01 -0600

    Now the proxy does GETs and POSTs and doesn't screw up the headers

--- proxy.py
+++ proxy.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import io
 import logging
@@ -30,10 +28,75 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                 hostname = self.headers.get("Host")
                 if not hostname:
                     hostname = "localhost"
-                print("The hostname is:")
-                print(hostname)
                 url = "http://{}".format(hostname)
-                print(url)
+                req = urllib.request.Request(url=url)
+                sio.write("====BEGIN REQUEST=====\n")
+                sio.write(url)
+                sio.write("\n")
+                sio.write(self.command)
+                sio.write(" ")
+                sio.write(self.path)
+                sio.write(" ")
+                sio.write(self.request_version)
+                sio.write("\n")
+                print("TRYING TO SEND HEADERS")
+                print(self.headers)
+                for line in self.headers:
+                    key = line
+                    value = self.headers.get(key)
+
+                    line_parts = [key, value]
+                    print(f"GET REQUEST HEADER: {line}")
+                    if len(line_parts) == 2:
+                        if line_parts[0].startswith("X-"):
+                            pass
+                        elif line_parts[0] in ("Connection", "User-Agent"):
+                            pass
+                        else:
+                            sio.write(line)
+                            req.add_header(key=key, val=value)
+                sio.write("\n")
+                sio.write("====END REQUEST=======\n")
+                logger.error(sio.getvalue() + "\n")
+                try:
+                    resp = urllib.request.urlopen(req)
+                except urllib.error.HTTPError as e:
+                    if e.getcode():
+                        resp = e
+                    else:
+                        self.send_error(599, "error proxying: {}".format(str(e)))
+                        sent = True
+                        return
+                self.send_response(resp.getcode())
+                headers = resp.getheaders()
+                for header in headers:
+                    name, value = header
+                    print(f"HEADER BEING WRITTEN TO BE SENT TO CLIENT: {name}: {value}")
+                    self.send_header(keyword=name, value=value)
+                self.end_headers()
+                sent = True
+                if body:
+                    self.wfile.write(resp.read())
+                return
+            finally:
+                if resp:
+                    resp.close()
+                sio.close()
+        except IOError as e:
+            if not sent:
+                self.send_error(404, "error trying to proxy: {}".format(str(e)))
+
+    def do_POST(self, body=True):
+        sent = False
+        try:
+            req = None
+            resp = None
+            sio = io.StringIO()
+            try:
+                hostname = self.headers.get("Host")
+                if not hostname:
+                    hostname = "localhost"
+                url = "http://{}".format(hostname)
                 req = urllib.request.Request(url=url)
                 sio.write("====BEGIN REQUEST=====\n")
                 sio.write(url)
@@ -45,8 +108,10 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                 sio.write(self.request_version)
                 sio.write("\n")
                 for line in self.headers:
-                    line_parts = [o.strip() for o in line.split(":", 1)]
-                    print(line_parts)
+                    key = line
+                    value = self.headers.get(key)
+                    print(f"HEADER BEING WRITTEN TO BE SENT TO TARGET: {key}")
+                    line_parts = [key, value]
                     if len(line_parts) == 2:
                         if line_parts[0].startswith("X-"):
                             pass
@@ -54,14 +119,12 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                             pass
                         else:
                             sio.write(line)
-                            req.add_header(*line_parts)
+                            req.add_header(key=key, val=value)
                 sio.write("\n")
                 sio.write("====END REQUEST=======\n")
-                logger.error(sio.getvalue())
+                logger.error(sio.getvalue() + "\n")
+                logger.error("No real error\n")
                 try:
-                    print("req is")
-                    print(req)
-                    print(req.get_full_url())
                     resp = urllib.request.urlopen(req)
                 except urllib.error.HTTPError as e:
                     if e.getcode():
@@ -71,11 +134,11 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                         sent = True
                         return
                 self.send_response(resp.getcode())
-                respheaders = resp.info()
-                for line in resp.headers:
-                    line_parts = line.split(":", 1)
-                    if len(line_parts) == 2:
-                        self.send_header(*line_parts)
+                headers = resp.getheaders()
+                for header in headers:
+                    name, value = header
+                    print(f"HEADER BEING WRITTEN TO BE SENT TO CLIENT: {name}: {value}")
+                    self.send_header(keyword=name, value=value)
                 self.end_headers()
                 sent = True
                 if body:
@@ -96,15 +159,8 @@ def parse_args(argv=sys.argv[1:]):
         "--port",
         dest="port",
         type=int,
-        default=random.randint(20000, 60000),
+        default=8181,
         help="serve HTTP requests on specified port (default: random)",
-    )
-    parser.add_argument(
-        "--type",
-        dest="server_type",
-        choices=["echo", "proxy"],
-        default="echo",
-        help="Whether to run as a proxy server or echo server",
     )
     args = parser.parse_args(argv)
     return args
@@ -114,11 +170,8 @@ def main(argv=sys.argv[1:]):
     args = parse_args(argv)
     print(("http server is starting on port {}...".format(args.port)))
     server_address = ("127.0.0.1", args.port)
-    if args.server_type == "proxy":
-        httpd = HTTPServer(server_address, ProxyHTTPRequestHandler)
-    else:
-        httpd = HTTPServer(server_address, EchoHTTPRequestHandler)
-    print(("http server is running as {}...".format(args.server_type)))
+    httpd = HTTPServer(server_address, ProxyHTTPRequestHandler)
+    print("http server is running as proxy")
     httpd.serve_forever()
 
 
